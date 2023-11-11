4c1c1b2ad376ff9a57822a6509bfe55c7e625024
Jafar Asad <rafajdasa@gmail.com>
2023-11-11 14:39:28 +0300

    initialized project files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  const myname = "Ja'far"
+
+  return (
+    <div>
+      
+      <h1>Hello {myname}</h1>
+      <p>this is my first react project, not really...</p>
+    
+    </div>
+  )
+}
+
+export default App
