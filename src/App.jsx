a4bb132cfa687f336d9a4a4f25051ea1e56a3715
Jafar Asad <rafajdasa@gmail.com>
2023-12-09 08:06:13 +0300

    using MAP

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,25 @@ function App() {
   const myname1 = "Ja'far"
   const myname2 = "Musa"
 
+  // const colors = ["red", "blue", "green", "yellow"]
+
+  const users = [
+    {name: "Musa", color: "red", id: 0, country: "Senegal"}, 
+    {name: "Isa", color: "blue", id: 1, country: "Morocco"}, 
+    {name: "Ali", color: "green", id:2, country: "Turkey"}
+  ]
+
   return (
     <div>
-    <Counter />
+
+      {users.map(item => <Hello user={item} key={item.id} />)}
+      {/* {users.map(item => <Hello name={item} color="red" />)} */}
+
+
+    {/* <Counter /> */}
       {/* <Hello name={myname1} color="red" />
       <Hello name={myname2} color="blue" /> */}
+      {/* {colors.map((item, index) =><p key={index}>{item}</p>)} */}
     
     </div>
   )
