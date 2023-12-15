c79e7a3e601779e24643b79a33a477359179d7b9
Jafar Asad <rafajdasa@gmail.com>
2023-12-15 08:47:38 +0300

    routed to different pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,35 +2,25 @@ import './App.css'
 import Hello from './components/Hello/Hello'
 import Counter from './components/Counter/Counter'
 import Welcome from './components/Welcome/Welcome'
+import Header from './components/Header/Header';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Products from './pages/Products/Products';
+
 
 function App() {
 
-  const myname1 = "Ja'far"
-  const myname2 = "Musa"
-
-  // const colors = ["red", "blue", "green", "yellow"]
-
-  const users = [
-    {name: "Musa", color: "red", id: 0, country: "Senegal"}, 
-    {name: "Isa", color: "blue", id: 1, country: "Morocco"}, 
-    {name: "Ali", color: "green", id:2, country: "Turkey"}
-  ]
-
   return (
-    <div>
-
-      <Welcome />
-
-      {/* {users.map(item => <Hello user={item} key={item.id} />)} */}
-      {/* {users.map(item => <Hello name={item} color="red" />)} */}
-
-
-    {/* <Counter /> */}
-      {/* <Hello name={myname1} color="red" />
-      <Hello name={myname2} color="blue" /> */}
-      {/* {colors.map((item, index) =><p key={index}>{item}</p>)} */}
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/products' element={<Products />} />
+      </Routes>
     
-    </div>
+    </BrowserRouter>
   )
 }
 
